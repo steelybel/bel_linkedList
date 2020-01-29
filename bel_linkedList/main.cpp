84185753ff8986fb3907defd2e9fd47ec5cf8197
steelybel <bel.bradberry26@gmail.com>
2020-01-29 09:24:13 -0800

    Worked more on exercise, linked list mooostly functions

--- bel_linkedList/main.cpp
+++ bel_linkedList/main.cpp
@@ -1,6 +1,31 @@
 #include <stdlib.h>
+#include <iostream>
 #include "tLinkedList.h"
 int main()
 {
+	tForwardList<int> list;
+	bool go = true;
+	int input = 0;
+	int step = 0;
+	while (go)
+	{
+		std::cin >> input;
+		std::cout << std::endl;
+		list.push_front(input);
+		step++;
+		if (step == 5) go = false;
+		std::cin.ignore();
+	}
+	list.remove(23);
+	list.remove(24);
+	list.pop_front();
+	list.pop_front();
+	list.pop_front();
+	if (list.empty())
+	{
+		std::cout << "yooo wtf" << std::endl;
+	}
+	std::cin >> input;
+	std::cout << std::endl;
 	return 0;
 }
--- bel_linkedList/tLinkedList.h
+++ bel_linkedList/tLinkedList.h
@@ -11,6 +11,23 @@ class tForwardList
 	};
 
 	Node * head;                    // pointer to head of linked list
+	class iterator
+	{
+		Node * cur;                                 // current node being operated upon
+
+	public:
+		iterator();                                 // initializes an empty iterator pointing to null
+		iterator(Node * startNode);                 // initializes an iterator pointing to the given node
+
+		bool operator==(const iterator& rhs) const; // returns true if the iterator points to the same node
+		bool operator!=(const iterator& rhs) const; // returns false if the iterator does not point to the same node
+		T& operator*() const;                       // returns a reference to the element pointed to by the current node
+		iterator& operator++();                     // pre-increment (returns a reference to this iterator after it is incremented)
+		iterator operator++(int);                   // post-increment (returns an iterator to current node while incrementing the existing iterator)
+	};
+
+	iterator begin();
+	iterator end();
 
 public:
 	tForwardList();                 // initializes head to null
@@ -23,6 +40,11 @@ public:
 	const T& front() const;         // returns the element at the head (const)
 
 	void remove(const T& val);      // removes all elements equal to the given value
+
+	bool empty() const;             // Returns true if there are no elements
+	void clear();                   // Destroys every single node in the linked list
+	void resize(size_t newSize);    // Resizes the linked list to contain the given number of elements
+									// New elements are default-initialized
 };
 
 template<typename T>
@@ -40,9 +62,9 @@ inline tForwardList<T>::~tForwardList()
 template<typename T>
 inline void tForwardList<T>::push_front(const T & val)
 {
-	Node N = new Node;
-	N.data = val;
-	N.next = head;
+	Node * N = new Node();
+	N->data = val;
+	N->next = head;
 	head = N;
 }
 
@@ -51,7 +73,7 @@ inline void tForwardList<T>::pop_front()
 {
 	if (head != nullptr)
 	{
-		Node N = head;
+		Node * N = head;
 		head = head->next;
 		delete N;
 	}
@@ -72,14 +94,56 @@ inline const T & tForwardList<T>::front() const
 template<typename T>
 inline void tForwardList<T>::remove(const T & val)
 {
+	while (head->data == val || head == nullptr)
+	{
+		pop_front();
+	}
 	Node * it = head;
-	while ((it.next != nullptr) && (it.next.data != val))
+	while (it->next != nullptr)
 	{
-		it = it.next;
+		if (it->next->data == val)
+		{
+			Node * deletus = it->next;
+			it->next = it->next->next;
+			delete deletus;
+		}
+		else
+		{
+			it = it->next;
+		}
 	}
-	if (it.next.data == val)
+}
+
+template<typename T>
+inline bool tForwardList<T>::empty() const
+{
+	if (head == nullptr) {	return true; }
+	return false;
+}
+
+template<typename T>
+inline tForwardList<T>::iterator::iterator()
+{
+	cur = nullptr;
+}
+
+template<typename T>
+inline tForwardList<T>::iterator::iterator(Node * startNode)
+{
+	cur = &startNode;
+}
+
+template<typename T>
+inline bool tForwardList<T>::iterator::operator==(const iterator & rhs) const
+{
+	while (cur != rhs.cur)
 	{
-		it = it.next;
-		delete it;
+		if (cur.next != nullptr)
+		{
+			cur = cur.next;
+		}
 	}
+	if (cur == rhs.cur) return true;
+	else return false;
+	
 }
